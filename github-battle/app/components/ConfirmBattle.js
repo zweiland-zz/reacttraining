76fb2a4ea749706d2e816a04a12aa4fc8feb354d
Zak Weiland <zweiland@gmail.com>
2017-01-02 11:07:26 -0600

    Rendering UI Lesson

--- github-battle/app/components/ConfirmBattle.js
+++ github-battle/app/components/ConfirmBattle.js
@@ -1,48 +1,40 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-var transparentBg = require('../styles').transparentBg;
+var styles = require('../styles');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
+var UserDetailsWrapper = require('./UserDetailsWrapper');
+var UserDetails = require('./UserDetails');
 
-function puke (object) {
-  return <pre>{JSON.stringify(object, null, '')}</pre>
-}
-
-function ConfirmBattle(props) {
+function ConfirmBattle (props) {
   return props.isLoading === true
-  ? <p> LOADING! </p>
-  : <div className="jumbotron col-sm-12 text-center">
-      <h1>Confirm Players</h1>
-      <div className='col-sm-8 col-sm-offset-2'>
-        <div className='col-sm-6'>
-          <p className='lead'>Player 1</p>
-          {puke(props.playersInfo[0])}
+    ? <p>LOADING</p>
+    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <UserDetailsWrapper header='Player 1'>
+            <UserDetails info={props.playersInfo[0]} />
+          </UserDetailsWrapper>
+          <UserDetailsWrapper header='Player 2'>
+            <UserDetails info={props.playersInfo[1]} />
+          </UserDetailsWrapper>
         </div>
-        <div className='col-sm-6'>
-          <p className='lead'>Player 2</p>
-          {puke(props.playersInfo[1])}
-        </div>
-      </div>
-      <div className='col-sm-8 col-sm-offset-2'>
-        <div className='col-sm-12'>
-          <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
-            START the BATTLE!
-          </button>
-        </div>
-        <div className='col-sm-12'>
-          <Link to='/playerOne'>
-            <button type='button' className='btn btn-lg btn-danger'>
-              Switch Players
-            </button>
-          </Link>
+        <div className='col-sm-8 col-sm-offset-2'>
+          <div className='col-sm-12' style={styles.space}>
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate Battle!</button>
+          </div>
+          <div className='col-sm-12' style={styles.space}>
+            <Link to='/playerOne'>
+              <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
 }
 
-ConfirmBattle.PropTypes = {
+ConfirmBattle.propTypes = {
   isLoading: PropTypes.bool.isRequired,
-  handleInitiateBattle: PropTypes.func.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
   playersInfo: PropTypes.array.isRequired
 }
 
--- github-battle/app/containers/ConfirmBattleContainer.js
+++ github-battle/app/containers/ConfirmBattleContainer.js
@@ -1,27 +1,23 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
-var githubHelpers = require('../utils/githubHelpers')
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
   getInitialState: function () {
-    console.log('getInitialState');
     return {
       isLoading: true,
-      playersInfo: []
+      playersInfo: [],
     }
-  },
-  componentWillMount: function () {
-    console.log('componentWillMount');
   },
   componentDidMount: function () {
     var query = this.props.location.query;
     githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
       .then(function (players) {
         this.setState({
-          isLoading:false,
+          isLoading: false,
           playersInfo: [players[0], players[1]]
         })
       }.bind(this))
@@ -40,7 +36,7 @@ var ConfirmBattleContainer = React.createClass({
         isLoading={this.state.isLoading}
         onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo} />
-    );
+    )
   }
 });
 
